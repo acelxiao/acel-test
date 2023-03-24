79f90c1d77b72a0c8c831c0a972c4bb2171c8b66
xiaoxiang1 <xiaoxiang1@joyy.com>
2023-03-25 00:00:00 +0800

    动态加载类

--- common-test/src/main/java/com/acel/notspring/groovy/Test.java
+++ common-test/src/main/java/com/acel/notspring/groovy/Test.java
@@ -1,6 +1,7 @@
 package com.acel.notspring.groovy;
 
 import com.acel.notspring.javacode.DynamicClassLoader;
+import com.acel.spring.hotReplaceBean.HelloService;
 import groovy.lang.GroovyClassLoader;
 import org.codehaus.groovy.control.CompilerConfiguration;
 
@@ -19,7 +20,8 @@ public class Test {
     public static void main(String[] args) throws UnsupportedEncodingException {
         String base64JavaCode = "cGFja2FnZSBjb20uYWNlbC5zcHJpbmcuaG90UmVwbGFjZUJlYW47CgppbXBvcnQgb3JnLnNwcmluZ2ZyYW1ld29yay5zdGVyZW90eXBlLlNlcnZpY2U7CgpAU2VydmljZQpwdWJsaWMgY2xhc3MgSGVsbG9TZXJ2aWNlIGltcGxlbWVudHMgTXkgewogICAgQE92ZXJyaWRlCiAgICBwdWJsaWMgU3RyaW5nIHRlc3QoKXsKICAgICAgICByZXR1cm4gIm5ldyI7CiAgICB9Cn0K";
         String javaCode = new String(Base64.getDecoder().decode(base64JavaCode), "UTF-8");
+        System.out.println(HelloService.class.getClassLoader());
         Class<?> clz = groovy.parseClass(javaCode);
-        System.out.println(clz.getName());
+        System.out.println(clz.getClassLoader());
     }
 }
